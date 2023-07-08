80a8ad59a6a55b9c03dcc10dcfe91ec5f848be5a
Vasily Korovin <vskorovin@gmail.com>
2023-07-08 10:02:03 +0300

    Bot project final

--- bot/hw15bot.py
+++ bot/hw15bot.py
@@ -96,7 +96,10 @@ async def get_birthday(message: types.Message, state: FSMContext):
 async def main():
     await dp.start_polling(bot)
 
+def run():
+    asyncio.run(main())
 
 if __name__ == '__main__':
-     asyncio.run(main())
+    run()
+
 
